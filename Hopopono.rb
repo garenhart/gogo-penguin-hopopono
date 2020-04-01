6ddfa2fb0240a6f9725a1b6d7aca69b5c1a803fd
Garen Hartunian <ghartunian@gmail.com>
2020-04-01 16:31:30 -0400

    Kick sample change. Tempo change after section A.

--- Hopopono.rb
+++ Hopopono.rb
@@ -1,7 +1,7 @@
 # GO GO PENGUIN - HOPOPONO
 
 use_debug true
-use_bpm 92
+use_bpm 90
 
 # mixer
 amp_master = 1
@@ -18,7 +18,7 @@ gogo_chord3 = [:E2, :B2, :G3]
 gogo_chord4 = [:C2, :G2, :E3]
 
 define :gogo_kick do
-  sample :bd_tek
+  node_kick = sample :bd_tek
 end
 
 define :gogo_snare do
@@ -31,7 +31,7 @@ end
 
 with_fx :reverb, room: 0.6, amp: amp_master*amp_kick, mix: 0.5 do
   live_loop :drum_kick do
-    16.times do |i|
+    18.times do |i|
       bar = i+1
       puts "bar %d" %bar
       gogo_kick
@@ -52,15 +52,15 @@ with_fx :reverb, room: 0.6, amp: amp_master*amp_kick, mix: 0.5 do
         sleep 1
       end
     end
-    
-    # bar 17
-    
+    gogo_kick
+    use_bpm 102
+    sleep 14 # 22
   end
 end
 
 with_fx :reverb, room: 0.6, amp: amp_master*amp_snare, mix: 0.5 do
   live_loop :drum_snare do
-    16.times do
+    18.times do
       sleep 1
       gogo_snare
       sleep 0.75
@@ -71,43 +71,8 @@ with_fx :reverb, room: 0.6, amp: amp_master*amp_snare, mix: 0.5 do
       sleep 0.25
       sleep 1
     end
-    
-    # bar 17
-    
-  end
-end
-
-with_fx :reverb, room: 0.8, amp: amp_master*amp_piano_left, mix: 0.7 do
-  live_loop :piano_left do
-    use_synth :piano
-    sleep 16
-    4.times do
-      play_chord gogo_chord1, sustain: 4
-      sleep 4
-      play_chord gogo_chord2, sustain: 1.75
-      sleep 1.75
-      play_chord gogo_chord3, sustain: 2.25
-      sleep 2.25
-      play_chord gogo_chord4, sustain: 8
-      sleep 8
-    end
-  end
-end
-
-with_fx :reverb, room: 0.8, amp: amp_master*amp_piano_right, mix: 0.6 do
-  live_loop :piano_right do
-    use_synth :piano
-    sleep 48
-    2.times do
-      play :A5, sustain: 4
-      sleep 4
-      play :D6, sustain: 1.75
-      sleep 1.75
-      play :C6, sustain: 2.25
-      sleep 2.25
-      play :B5, sustain: 8
-      sleep 8
-    end
+    use_bpm 102
+    sleep 14 #22
   end
 end
 
@@ -115,7 +80,7 @@ with_fx :reverb, room: 0.9, amp: amp_master*amp_bass, mix: 0.4 do
   live_loop :bass do
     use_synth :fm
     sleep 16
-    4.times do
+    4.times do |i|
       play :G3, release: 4
       sleep 4
       play :A3, release: 1.75
@@ -123,7 +88,64 @@ with_fx :reverb, room: 0.9, amp: amp_master*amp_bass, mix: 0.4 do
       play :E3, release: 2.25
       sleep 2.25
       play_chord [:C2, :E3], sustain_level: 1.2, release: 8
+      if (i == 3)
+        use_bpm 102
+      end
       sleep 8
+    end
+    sleep 6 #22
+  end
+end
+
+
+with_fx :reverb, room: 0.8, amp: amp_master*amp_piano_left, mix: 0.6 do
+  live_loop :piano_left do
+    use_synth :piano
+    sleep 16
+    4.times do |i|
+      play_chord gogo_chord1, sustain: 3
+      sleep 4
+      play_chord gogo_chord2, sustain: 0.75
+      sleep 1.75
+      play_chord gogo_chord3, sustain: 1.25
+      sleep 2.25
+      play_chord gogo_chord4, sustain: 7
+      if (i == 3)
+        use_bpm 102
+      end
+      sleep 8
+    end
+    sleep 6
+  end
+end
+
+
+# right hand pattern sets
+rh_pattern = [[:b4, :b4, :d5, :b4], [:fs5, :g5, :b4, :g5], [:e5, :b4, :b4, :e5]]
+
+with_fx :reverb, room: 0.8, amp: amp_master*amp_piano_right, mix: 0.6 do
+  live_loop :piano_right do
+    use_synth :piano
+    sleep 48
+    2.times do |i|
+      play :A5, sustain: 4
+      sleep 4
+      play :D6, sustain: 1.75
+      sleep 1.75
+      play :C6, sustain: 2.25
+      sleep 2.25
+      play :B5, sustain: 8
+      puts "loop %d" %i
+      if (i==0)
+        sleep 8
+      else # end of second time
+        use_bpm 102
+        14.times do
+          play_pattern_timed rh_pattern.tick, [0.25]
+        end
+      end
     end
   end
 end
+
+
